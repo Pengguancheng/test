7506cb62cc0ef38c3e370aa51455773a71f079ec
Zoe hsu <jyun55520@gmail.com>
2016-10-18 14:36:48 +0800

    1.10.18 interface

--- CBee.js
+++ CBee.js
@@ -1,14 +1,97 @@
 var pointid;
 var imagesrc="ppp1.jpg";
-var stylesArray = [{"featureType":"water","elementType":"all","stylers":[{"hue":"#7fc8ed"},{"saturation":55},{"lightness":-6},{"visibility":"on"}]},{"featureType":"water","elementType":
-    "labels","stylers":[{"hue":"#7fc8ed"},{"saturation":55},{"lightness":-6},{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":
-    [{"hue":"#83cead"},{"saturation":1},{"lightness":-15},{"visibility":"on"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"hue":"#f3f4f4"},
-    {"saturation":-84},{"lightness":59},{"visibility":"on"}]},{"featureType":"landscape","elementType":"labels","stylers":[{"hue":"#ffffff"},{"saturation":-100},
-    {"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},
-    {"visibility":"on"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbbbbb"},{"saturation":-100},{"lightness":26},{"visibility":"on"}]},
-    {"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#ffcc00"},{"saturation":100},{"lightness":-35},{"visibility":"simplified"}]},
-    {"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#ffcc00"},{"saturation":100},{"lightness":-22},{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":
-    [{"hue":"#d7e4e4"},{"saturation":-60},{"lightness":23},{"visibility":"on"}]}]    /*地圖樣式變更*/
+var stylesArray =  [
+                      {
+                          "featureType": "water",
+                          "stylers": [
+                              {
+                                  "visibility": "on"
+                              },
+                              {
+                                  "color": "#b5cbe4"
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "landscape",
+                          "stylers": [
+                              {
+                                  "color": "#efefef"
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "road.highway",
+                          "elementType": "geometry",
+                          "stylers": [
+                              {
+                                  "color": "#83a5b0"
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "road.arterial",
+                          "elementType": "geometry",
+                          "stylers": [
+                              {
+                                  "color": "#bdcdd3"
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "road.local",
+                          "elementType": "geometry",
+                          "stylers": [
+                              {
+                                  "color": "#ffffff"
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "poi.park",
+                          "elementType": "geometry",
+                          "stylers": [
+                              {
+                                  "color": "#e3eed3"
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "administrative",
+                          "stylers": [
+                              {
+                                  "visibility": "on"
+                              },
+                              {
+                                  "lightness": 33
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "road"
+                      },
+                      {
+                          "featureType": "poi.park",
+                          "elementType": "labels",
+                          "stylers": [
+                              {
+                                  "visibility": "on"
+                              },
+                              {
+                                  "lightness": 20
+                              }
+                          ]
+                      },
+                      {},
+                      {
+                          "featureType": "road",
+                          "stylers": [
+                              {
+                                  "lightness": 20
+                              }
+                          ]
+                      }
+                  ]    /*地圖樣式變更*/
     var mapOptions = {
         center: { lat: 23.5, lng: 121},   /*台灣座標*/
         zoom: 8,
